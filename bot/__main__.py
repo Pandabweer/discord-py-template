74be7cf1584c96de6f06ce8f8f2bbc92ab8cc99d
miki55555115 <miki55555115@gmail.com>
2023-08-14 12:46:42 +0200

    feat(bot-base): create BotBase class and instance

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,5 +1,9 @@
+from bot.bot import BotBase
 from bot.constants import LoggingConfig
 from bot.log import get_logger
 
 log = get_logger("bot")
 log.debug(LoggingConfig.debug)
+
+bot = BotBase()
+bot.run()
---
+++ bot/bot.py
@@ -0,0 +1,19 @@
+from discord import Intents
+from discord.ext import commands
+from discord.ext.commands import HelpCommand
+
+from bot.constants import BotConfig
+
+
+class BotBase(commands.Bot):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(
+            *args,
+            command_prefix=BotConfig.prefix,
+            description=BotConfig.description,
+            help_command=HelpCommand() if BotConfig.default_help_command else None,
+            **kwargs,
+        )
+
+    async def run(self, *args, **kwargs) -> None:
+        super().run(BotConfig.token, *args, **kwargs)
